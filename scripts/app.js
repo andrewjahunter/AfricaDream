9831e4c457b8591e046d09b5b31702bd0df0cadf
Andrew Hunter <andrew@thehunters.co.za>
2014-11-01 15:00:49 +0200

    sss

--- scripts/app.js
+++ scripts/app.js
@@ -60,7 +60,7 @@ window.gdd = function () {
     //the thing to change for each news app is the appCode, its waht is used to configure the application
     var pledgeCode = 'africadream'
 
-    var headers = { "apiKey": "39251c1b-7585-476e-a69f-bbee4d17dd63", "appInfo": "appcode:5001|version:1.0.1" }
+    var headers = { "apiKey": "39251c1b-7585-476e-a69f-bbee4d17dd63", "appInfo": "appcode:5001|version:1.0.2" }
     var baseUrl = function () {
         //return "http://localhost/webapi2/api/"
         //return "http://192.168.0.2/webapi2/api/"
